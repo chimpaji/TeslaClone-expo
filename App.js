b2550ff6fc2f63ae58fc1eb1dc5125786a230136
chimpaji <chimpaji@outlook.com>
2021-09-10 22:35:40 +0700

    add imageBackground

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <ImageBackground
+          source={require("./assets/images/ModelX.jpeg")}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <View style={styles.titles}>
+            <Text style={styles.title}>Model S</Text>
+            <Text style={styles.subtitle}>Starting at $69,420</Text>
+          </View>
+        </ImageBackground>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  carContainer: {
+    height: "100%",
+    width: "100%",
+  },
+  titles: {
+    width: "100%",
+    alignItems: "center",
+    marginTop: "30%",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
